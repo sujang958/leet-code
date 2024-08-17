8005d719378cc819d78387d30d85b19a9f5f5045
sujang958 <sujangdev@gmail.com>
2024-08-17 20:34:47 +0900

    feat: try 28

---
+++ easy/find-the-index-of-the-first-occurrence-in-a-string/solve.ts
@@ -0,0 +1,10 @@
+function strStr(haystack: string, needle: string): number {
+  // return haystack.indexOf(needle) too ez
+
+  let answer = -1
+
+  for (let i = 0; i < haystack.length; i++)
+    if (haystack.slice(i, i + needle.length) == needle) answer = i
+
+  return answer
+}
